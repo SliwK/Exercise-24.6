2b9853bbd52a7f0c997f3c47773f64948a77d827
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-04-04 00:38:06 +0200

    Rest of tests

--- src/App.test.js
+++ src/App.test.js
@@ -14,8 +14,41 @@ it('renders without crashing', () => {
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it("should change player's score", () => {
+
+  const appComponent = shallow(<App />);
+  const players = [{
+    name: 'Kunegunda',
+    score: 5
+  }]
+
+  appComponent.setState({ players });
+
+  const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+  onScoreUpdate(0, 5);
+
+  const playersAfterUpdate = appComponent.state().players;
+  expect(playersAfterUpdate[0].score).toEqual(10);
+});
+
+
+it("it should add a new player", () => {
+  const appComponent = shallow(<App />);
+
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Ania');
+
+  const players = appComponent.state('players');
+
+  expect(players.length).toEqual(1);
+  expect(players[0].name).toEqual('Ania');
+  expect(players[0].score).toEqual(0);
 });
